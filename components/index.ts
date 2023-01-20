207f09d05abde0bf6ea60482ba684cea697ff009
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-01-20 15:54:15 +0300

    111

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,4 @@ export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './P/P';
 export * from './Tag/Tag';
-export * from './Rating/Rating';
 export * from './MenuItem/MenuItem';
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import styles from '../styles/Home.module.css';
 import Button from '../components/Button/Button';
 import Htag from '../components/Htag/Htag';
 import P from '../components/P/P';
-import Rating from '../components/Rating/Rating';
 import Tag from '../components/Tag/Tag';
 import { withLayout } from '../layout/Layout';
 import { UserItem } from '../interfaces/menu.interface';
