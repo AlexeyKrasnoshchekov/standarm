30c002655f1c410cd031c0ee0a072ad182ed4a85
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-01-22 10:57:37 +0300

    news item

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -1,10 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import styles from '../../styles/Post.module.css';
-// import Container from '../../components/Container';
+import styles from '../../styles/NewsItem.module.css';
+import Image from 'next/image';
+import parse from 'html-react-parser';
+import Link from 'next/link';
 
 function Post(post: any) {
-  //   console.log('999', post.post.id);
-  return <h3>{post.post.title}</h3>;
+  // const parser = new DOMParser();
+  const htmlContent = post.post.content && parse(post.post.content);
+
+  return (
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>{post.post.title}</h3>
+      <div className={styles.innerWrapper}>
+        <div className={styles.imageWrapper}>
+          {post.post.images.length !== 0 &&
+            post.post.images.map((image: any) => {
+              return (
+                <Image
+                  key={post.post.id}
+                  src={image}
+                  alt="фото новости"
+                  layout="fill"
+                  className={styles.image}
+                  style={{ objectFit: 'contain' }}
+                />
+              );
+            })}
+        </div>
+        <div className={styles.textWrapper}>
+          {post.post.id === 5 && (
+            <div className={styles.waterHeading}>
+              <span>Мы рады представить Вам новый раздел нашего сайта:</span>
+              <Link className={styles.waterLink} href={'/otr/5'}>
+                Технологии очистки воды
+              </Link>
+            </div>
+          )}
+          {htmlContent && htmlContent}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default withLayout(Post);
@@ -31,6 +67,7 @@ export async function getStaticProps(context: any) {
   //   console.log('444', objectData);
 
   const post = objectData.posts.filter((post: Post) => post.id == id)[0];
+  if (post.id === 1) post.images.shift();
   //   console.log('555', post);
   if (post === 0) {
     return {
