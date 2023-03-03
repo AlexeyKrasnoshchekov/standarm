11c6ca1f53bf2d7159f155bde1fcb71897a74259
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-03-03 16:13:22 +0300

    404 styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,31 @@
 import { withLayout } from '@/layout/Layout';
-import React from 'react'
+import React from 'react';
+import '../styles/404.scss';
 
-type Props = {}
+type Props = {};
 
 export function Error404({}: Props) {
   return (
-    <div>Ошибка 404</div>
-  )
+    <div className="flex-container">
+      <div className="text-center">
+        <h1>
+          <span className="fade-in" id="digit1">
+            4
+          </span>
+          <span className="fade-in" id="digit2">
+            0
+          </span>
+          <span className="fade-in" id="digit3">
+            4
+          </span>
+        </h1>
+        <h3 className="fadeIn">Страница не найдена</h3>
+        <button type="button" name="button">
+          Вернуться на Домашнюю
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default withLayout(Error404);
