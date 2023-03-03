8f64f4f38810bdde0dba468be82e1f24ee367ed9
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-03-03 16:56:40 +0300

    404 finished

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,29 +1,25 @@
 import { withLayout } from '@/layout/Layout';
+import Link from 'next/link';
 import React from 'react';
-import '../styles/404.scss';
+import styles from '../styles/404.module.css';
 
 type Props = {};
 
 export function Error404({}: Props) {
   return (
-    <div className="flex-container">
-      <div className="text-center">
-        <h1>
-          <span className="fade-in" id="digit1">
-            4
-          </span>
-          <span className="fade-in" id="digit2">
-            0
-          </span>
-          <span className="fade-in" id="digit3">
-            4
-          </span>
-        </h1>
-        <h3 className="fadeIn">Страница не найдена</h3>
-        <button type="button" name="button">
-          Вернуться на Домашнюю
-        </button>
+    <div className={styles.mainbox}>
+      <div className={styles.inner}>
+        <div className={styles.err}>4</div>
+        <div className={styles.err}>0</div>
+        <div className={styles.err}>4</div>
       </div>
+      <div className={styles.error}>Ошибка</div>
+      <div className={styles.msg}>
+        Страница не найдена
+      </div>
+      <Link href={'/'} className={styles.link}>
+          Вернуться на домашнюю
+        </Link>
     </div>
   );
 }
